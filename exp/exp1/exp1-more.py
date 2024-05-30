9a23f84fd6babf480f29b95ce0068afadab31d5d
MelodicTechno <617179025@qq.com>
2024-05-30 23:09:49 +0800

    add info

--- exp/exp1/exp1-more.py
+++ exp/exp1/exp1-more.py
@@ -7,6 +7,7 @@ import time
 
 #从获取的网页源代码中提取目标数据
 def extract_data(html_code):
+    
     #目标数据的正则表达式
     p_job = 'class="jname text-cut">(.*?)</span>'
     p_salary = 'class="sal shrink-0">(.*?)</span>'
@@ -30,12 +31,13 @@ def extract_data(html_code):
     #用上面的字典创建一个DataFrame
     return pd.DataFrame(data_dt)
 def get_pages(keyword, city, start, end):
+    
     # 声明要模拟的浏览器是Chrome,并启用无界面浏览模式
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     browser = webdriver.Chrome(options=chrome_options)
     # wait 10s if necessary
-    browser.implicitly_wait(10)
+    # browser.implicitly_wait(10)
     # browser.maximize_window()
 
     # 通过get()函数控制浏览器发起请求，访问网址,获取源码
@@ -45,12 +47,13 @@ def get_pages(keyword, city, start, end):
     browser.find_element(By.XPATH, '//*[@id="kwdselectid"]').clear()
     browser.find_element(By.XPATH, '//*[@id="kwdselectid"]').send_keys(keyword)
     browser.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/button').click()
-
+    
     # time.sleep(10)
     all_data = pd.DataFrame()
     for page in range(start, end + 1):
         # 模拟人操作浏览器，输入搜索关键词，点击搜索按钮
-        time.sleep(3)
+        pre_window = browser.current_window_handle
+        time.sleep(5)
         browser.find_element(By.CLASS_NAME, 'mytxt').clear()
         browser.find_element(By.CLASS_NAME, 'mytxt').send_keys(page)
         browser.find_element(By.CLASS_NAME, 'jumpPage').click()
@@ -59,6 +62,22 @@ def get_pages(keyword, city, start, end):
         #将提取的目标数据添加到DataFrame中
         # print(extract_data(browser.page_source))
         # all_data = all_data.append(extract_data(browser.page_source))
+        work_info_list = []
+        for i in range(1, 19):
+            xpath = f'//*[@id="app"]/div/div[2]/div/div/div[2]/div/div[2]/div/div[2]/div[1]/div[{i}]/div[2]/div[1]/span[1]'
+            link = browser.find_element(By.XPATH, xpath)
+            original_window = browser.current_window_handle
+            
+            link.click()
+            browser.switch_to.window(browser.window_handles[1])
+            time.sleep(3)
+            info_html = browser.page_source
+            p_info = '<div class="bmsg job_msg inbox">\n(.*?)<a track-type="NewTrackButtonClick"'
+            info = re.findall(p_info, info_html, re.S)
+            work_info_list = info
+            browser.close()
+            browser.switch_to.window(original_window)
+        # browser.switch_to.window(pre_window)
         all_data = pd.concat([all_data, extract_data(browser.page_source)])
         # extract_data(browser, browser.page_source)
 
