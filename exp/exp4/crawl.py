6424481fc999c19118d1645e605221327070be53
MelodicTechno <617179025@qq.com>
2024-06-01 00:22:38 +0800

    crawl

---
+++ exp/exp4/crawl.py
@@ -0,0 +1,28 @@
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+def run(web_path, output_path):
+    wd = webdriver.Chrome()
+    # wd.get(r'https://data.stats.gov.cn/easyquery.htm?cn=C01&zb=A030602&sj=2023')
+    wd.get(web_path)
+
+    time.sleep(3)
+
+    xpaths = [f'//*[@id="table_main"]/tbody/tr[{i}]/td[2]' for i in range(2, 22)]
+
+    nums = []
+
+    for xpath in xpaths:
+        num = wd.find_element(By.XPATH, xpath).text
+        nums.append(num)
+
+    data = {'数量': nums}
+    pd_data = pd.DataFrame(data)
+
+    # pd_data.to_csv(r'exp\exp4\male.csp')
+    pd_data.to_csv(output_path)
+
+    wd.close()
---
+++ exp/exp4/crawlmain.py
@@ -0,0 +1,4 @@
+from crawlmain import run
+
+run(r'https://data.stats.gov.cn/easyquery.htm?cn=C01&zb=A030602&sj=2023', r'exp\exp4\male.csv')
+run(r'https://data.stats.gov.cn/easyquery.htm?cn=C01&zb=A030602&sj=2023',r'exp\exp4\female.csv')
