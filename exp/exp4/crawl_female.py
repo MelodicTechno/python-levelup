bd4c38383668128fabf7ba8dd920dc43c1ca0503
MelodicTechno <617179025@qq.com>
2024-06-01 15:15:40 +0800

    add: crawl_female.py

---
+++ exp/exp4/crawl_female.py
@@ -0,0 +1,25 @@
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+wd = webdriver.Chrome()
+wd.get(r'https://data.stats.gov.cn/easyquery.htm?cn=C01&zb=A030603&sj=2023')
+
+time.sleep(3)
+
+xpaths = [f'//*[@id="table_main"]/tbody/tr[{i}]/td[2]' for i in range(2, 22)]
+
+nums = []
+
+for xpath in xpaths:
+    num = wd.find_element(By.XPATH, xpath).text
+    nums.append(num)
+
+data = {'数量': nums}
+pd_data = pd.DataFrame(data)
+
+pd_data.to_csv(r'exp\exp4\female.csp')
+
+wd.close()
