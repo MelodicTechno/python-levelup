3cf28385f6da83b5adf7fded16dad4d33ab11621
MelodicTechno <617179025@qq.com>
2024-06-03 09:35:01 +0800

    添加了实验2的输入文件，修改了代码里的路径

--- exp/exp2/exp2-worldcloud.py
+++ exp/exp2/exp2-worldcloud.py
@@ -1,4 +1,3 @@
-#Selenium模块是一个自动化测试工具，能够驱动浏览器模拟人的操作，如单击、键盘输入等。
 import re
 import jieba
 from jieba.analyse import *
@@ -7,35 +6,35 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 #读取excel文件工作表Sheet1
-workbook = load_workbook('职位.xlsx')
+workbook = load_workbook(r'exp\exp2\share\职位.xlsx')
 worksheet = workbook['Sheet1']
 #将Sheet1中F列内容写入txt文件
 data = []
-f = open('职位信息.txt', 'w')
+f = open(r'exp\exp2\output\职位信息.txt', 'w')
 for row in range(2, worksheet.max_row + 1):
-    data.append(worksheet['F'+str(row)].value)
+    data.append(worksheet['H'+str(row)].value)
 f.write(str(data))
 f.close()
 
 #去除html标签
-f = open('职位信息.txt', 'r').read()
+f = open(r'exp\exp2\output\职位信息.txt', 'r').read()
 g = re.sub('<.*?>', '', f)
 g = re.sub('nbsp', '', g)
 g = re.sub('  ', '', g)
 #清洗后数据写入新的txt
-h = open('职位信息-清洗后.txt', 'w')
+h = open(r'exp\exp2\output\职位信息-清洗后.txt', 'w')
 h.write(g)
 h.close()
 
 #生成词云
 #读取txt
-f = open('职位信息-清洗后.txt', 'r', encoding='gbk').read()
+f = open(r'exp\exp2\output\职位信息-清洗后.txt', 'w', 'r', encoding='gbk').read()
 #分词
 sep_list = jieba.lcut(f)
 print('分词', type(sep_list), len(sep_list), sep_list)
 #过滤停用词
 # stopwords为停用词list，stop.txt停用词一行一个
-stopwords = [line.strip() for line in open('stop.txt', 'r', encoding='utf-8').readlines()]
+stopwords = [line.strip() for line in open(r'exp\exp2\share\stop.txt', 'r', encoding='utf-8').readlines()]
 print('停用词', type(stopwords), stopwords)
 outstr = ''
 for word in sep_list:
@@ -51,7 +50,7 @@ print('再次分词后', type(outstr), outstr)
 font = r'C:\Windows\Fonts\simsun.ttc'
 wc = WordCloud(font_path=font, width=2400, height=1200, max_words=100)
 wc.generate(outstr)
-wc.to_file('词云.jpg')
+wc.to_file(r'exp\exp2\output\词云.jpg')
 plt.figure(dpi=100)
 plt.imshow(wc, interpolation='catrom')
 plt.axis('off')
