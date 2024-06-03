7987d6b58842a9dfb124d24c7e5c15f708a1cb48
MelodicTechno <617179025@qq.com>
2024-06-03 16:18:49 +0800

    添加角色到词云图里

--- exp/exp2/exp2-worldcloud.py
+++ exp/exp2/exp2-worldcloud.py
@@ -1,14 +1,23 @@
 import re
 import jieba
+from PIL import Image
 from jieba.analyse import *
 from openpyxl import load_workbook
 from wordcloud import WordCloud
+import wordcloud
+import matplotlib.pyplot as plt
+import numpy as np
 import matplotlib.pyplot as plt
 
+# jieba.suggest_freq('n', False)
+# jieba.suggest_freq('和', False)
+# jieba.suggest_freq('的', False)
+# jieba.suggest_freq('关键字', False)
+# jieba.suggest_freq('者', False)
 #读取excel文件工作表Sheet1
 workbook = load_workbook(r'exp\exp2\share\职位.xlsx')
 worksheet = workbook['Sheet1']
-#将Sheet1中F列内容写入txt文件
+#将Sheet1中H列内容写入txt文件
 data = []
 f = open(r'exp\exp2\output\职位信息.txt', 'w')
 for row in range(2, worksheet.max_row + 1):
@@ -49,7 +58,13 @@ print('再次分词后', type(outstr), outstr)
 #设置词云使用的字体
 font = r'C:\Windows\Fonts\simsun.ttc'
 wc = WordCloud(font_path=font, width=2400, height=1200, max_words=100)
+ar15 = np.array(Image.open(r'exp\exp4\ar15.png'))
+ar15_colors = wordcloud.ImageColorGenerator(ar15)
 wc.generate(outstr)
+
+fig, axes = plt.subplot(1, 3)
+
+
 wc.to_file(r'exp\exp2\output\词云.jpg')
 plt.figure(dpi=100)
 plt.imshow(wc, interpolation='catrom')
