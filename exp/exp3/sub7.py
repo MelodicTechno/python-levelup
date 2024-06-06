549476a36ac77dd73c7ac3843acb7231209c3da7
cs <617179025@qq.com>
2024-06-06 21:08:22 +0800

    modify sub7

--- exp/exp3/sub7.py
+++ exp/exp3/sub7.py
@@ -1,11 +1,13 @@
 import numpy as np
+import scipy as sp
 
 A = np.array([[1, -0.3], [-0.1, 0.9]])
 B = np.array([[1, 0.5], [0.7, -0.9]])
-evalues, evectors = np.linalg.eig(A)
+evalues, evectors = sp.linalg.eig(A, B)
 
 print(evalues)
 print(evectors)
 
-# [[ 0.91724574  0.79325185]
-#  [-0.3983218   0.60889368]]
+# [ 0.7367235+0.j -0.9447235+0.j]
+# [[-0.93041671  0.08828439]
+#  [-0.36650341 -0.99609531]]
