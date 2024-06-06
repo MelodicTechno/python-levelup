1d945d496d9464214d1f4517b3aa6d1f3888e0c5
cs <617179025@qq.com>
2024-06-06 19:16:54 +0800

    add sub2

--- exp/exp3/sub2.py
+++ exp/exp3/sub2.py
@@ -1,17 +1,12 @@
 import numpy as np
+from scipy import linalg
 
-x = np.arange(15, dtype=np.int64).reshape(3, 5)
-x[1: ,::2] = -99
-print(x)
+A = np.array([[1, 2], [3, 4]])
+print(A)
+A = linalg.inv(A)
+print(A)
 
-x.max(axis=1)
-
-rng = np.random.default_rng()
-samples = rng.normal(size=2500)
-print(samples)
-
-# output:
-
+# output
 # [[  0   1   2   3   4]
 #  [-99   6 -99   8 -99]
 #  [-99  11 -99  13 -99]]
