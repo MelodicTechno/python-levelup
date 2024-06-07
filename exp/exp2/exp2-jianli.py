34f5693487e4a36990fe32e0fdf7ebd115b39c6f
cs <617179025@qq.com>
2024-06-07 09:19:12 +0800

    add more file

--- exp/exp2/exp2-jianli.py
+++ exp/exp2/exp2-jianli.py
@@ -14,11 +14,12 @@ def info_update(doc, old_info, new_info):
             for cell in row.cells:
                 cell.text = cell.text.replace(old_info, new_info)
 #读取excel中的信息
-wb = load_workbook(r'share\简历信息.xlsx')
+wb = load_workbook(r'share/简历信息.xlsx')
 ws = wb.active
 #根据简历信息中的每行生成一个简历，即为每个公司生成一份专属简历
+file_num = 1
 for row in range(2, ws.max_row + 1):
-    doc = Document(r'share\简历模板.docx')
+    doc = Document(r'share/简历模板.docx')
     for col in range(1, ws.max_column + 1):
         #简历信息中的标题，对应word中的关键词
         old_info = str(ws.cell(row=1, column=col).value)
@@ -30,7 +31,8 @@ for row in range(2, ws.max_row + 1):
     #在word中的第2个表格的（0，1）单元格插入图片
     table = doc.tables[1]
     run = table.cell(0,1).paragraphs[0].add_run('')
-    run.add_picture(r'exp\exp2\output\qianzi-binary.png', width=Inches(1), height=Inches(0.5))
+    run.add_picture(r'exp/exp2/output/qianzi-binary.png', width=Inches(1), height=Inches(0.5))
     #基于更新后的简历模板生成以对应公司命名的简历
     com_name = str(ws.cell(row=row, column=1).value)
-    doc.save(r'exp\exp2\output\简历.docx')
+    doc.save(f'exp/exp2/output/简历{file_num}.docx')
+    file_num += 1
